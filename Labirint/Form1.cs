cdbec97af016c902b3c7cfbe2309a1cdb5f6e152
SilvaHaberl <silva.haberl1989@gmail.com>
2016-01-13 09:47:55 +0100

    Incijalizirati

---
+++ Labirint/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labirint
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+
+            InitializeComponent();
+            MoveToStart();
+
+        }
+
+
+        /// <summary>
+        /// Play a sound, then move the mouse pointer to a point 10 pixels down and to 
+        /// the right of the starting point in the upper-left corner of the maze.
+        /// </summary>
+        private void MoveToStart()
+        {
+         
+        }
+    }
+}
